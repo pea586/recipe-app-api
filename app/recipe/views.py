9678074ddf82a9c8b0e50fd73a3e945ee1caf647
bill <keepgoingbill@gmail.com>
2023-09-09 19:51:15 +1000

    recipe API part 2

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -24,4 +24,9 @@ class RecipeViewSet(viewsets.ModelViewSet):
         """Return the serializer class for request."""
         if self.action == 'list':
             return serializers.RecipeSerializer
+
         return self.serializer_class
+
+    def perform_create(self, serializer):
+        """Create a new recipe."""
+        serializer.save(user=self.request.user)
